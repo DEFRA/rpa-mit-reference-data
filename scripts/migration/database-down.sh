ca5f58fe2b1f0650b74e31a83c56d7d11f6c06a9
sfitz42 <shaun.r.fitzsimons@gmail.com>
2023-06-22 13:54:32 +0100

    Fix migrate / changelog

--- scripts/migration/database-down.sh
+++ scripts/migration/database-down.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "db update on $POSTGRES_HOST $SCHEMA_NAME as $SCHEMA_USERNAME"
-/scripts/postgres-wait && /liquibase/liquibase \
+/scripts/postgres-wait.sh && /liquibase/liquibase \
 --driver=org.postgresql.Driver \
 --changeLogFile=/changelog/db.changelog.xml \
 --url=jdbc:postgresql://$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB \
