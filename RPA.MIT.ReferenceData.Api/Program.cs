966795c2495d112165a620e13bfc9117f72f0050
Gibbons, Andy <Andy.Gibbons@defra.gov.uk>
2024-01-05 16:54:03 +0000

    Add SQL script generation during data seeding

--- RPA.MIT.ReferenceData.Api/Program.cs
+++ RPA.MIT.ReferenceData.Api/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using RPA.MIT.ReferenceData.Api;
 using RPA.MIT.ReferenceData.Api.Authentication;
 using RPA.MIT.ReferenceData.Api.Extensions;
 using RPA.MIT.ReferenceData.Api.SeedData;
@@ -8,14 +7,29 @@ using RPA.MIT.ReferenceData.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var interceptor = new AadAuthenticationInterceptor(new TokenGenerator(), builder.Configuration);
+SQLscriptWriter? _sqlScriptWriter = default;
+CreateAndInsertSqlCommandInterceptor? createAndInsertSQLCommandInterceptor = default;
+
+var isLocalDev = builder.Configuration.IsLocalDatabase(builder.Configuration);
+
+if (isLocalDev)
+{
+    _sqlScriptWriter = new SQLscriptWriter($"MIT_ReferenceData_Seed_SQL_{DateTime.Now.ToString("yyyyMMdd-HHmm")}.sql");
+    createAndInsertSQLCommandInterceptor = new CreateAndInsertSqlCommandInterceptor(_sqlScriptWriter);
+}
+
+var aadAuthenticationInterceptor = new AadAuthenticationInterceptor(new TokenGenerator(), builder.Configuration);
 
 builder.Services.AddDbContext<ReferenceDataContext>(options =>
 {
-    var connStringTask = interceptor.GetConnectionStringAsync();
+    var connStringTask = aadAuthenticationInterceptor.GetConnectionStringAsync();
     var connString = connStringTask.GetAwaiter().GetResult();
 
-    options.AddInterceptors(interceptor);
+    options.AddInterceptors(aadAuthenticationInterceptor);
+    if (isLocalDev && createAndInsertSQLCommandInterceptor is not null)
+    {
+        options.AddInterceptors(createAndInsertSQLCommandInterceptor);
+    }
 
     options.UseNpgsql(
         connString,
@@ -37,7 +51,7 @@ using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<ReferenceDataContext>();
 
-    SeedProvider.SeedReferenceData(db, builder.Configuration);
+    SeedProvider.SeedReferenceData(db, builder.Configuration, _sqlScriptWriter);
 }
 
 app.Run();
